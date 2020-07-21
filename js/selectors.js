949c429f9723a8efbff11bf660872cfd1252db01
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-21 01:36:16 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 // declare your functions here...
+  // good thing you said so, otherwise i was gonna slap them in index.html.
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  var varInternal = $('img');
+  console.log("-----------" + varInternal[0].innerHTML);
+  return "00000000000";
+}
+
+function ninjaBabySelector(){
+
+}
+
+function divSelector(){
+
+}
+
+function firstListItem(){
+
+}
